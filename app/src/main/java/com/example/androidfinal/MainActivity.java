b349b245d72b91f9652be4238869d38e9c70d6fa
liu00507 <liu00507@algonquinlive.com>
2020-12-01 13:13:23 -0500

    almost done need need to solve the crushing problem

--- app/src/main/java/com/example/androidfinal/MainActivity.java
+++ app/src/main/java/com/example/androidfinal/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.androidfinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button goToCovidBtn = findViewById(R.id.btn_to_Covid19Case);
+        Intent GoToCovidActivityIntent = new Intent(MainActivity.this, Covid19Case.class);
+        goToCovidBtn.setOnClickListener(click -> {
+            Toast.makeText(MainActivity.this, getResources().getString(R.string.covid_toast_message), Toast.LENGTH_LONG).show();
+            startActivity(GoToCovidActivityIntent);
+        });
     }
 }
---
+++ app/src/main/java/com/example/androidfinal/ProvinceDetailActivity.java
@@ -0,0 +1,52 @@
+package com.example.androidfinal;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProvinceDetailActivity extends AppCompatActivity {
+
+    String latitude;
+    String longitude;
+    String country;
+    String countryCode;
+    String provinceName;
+    String caseNumber;
+    String date;
+    long id;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_province_detail);
+
+        Intent fromIntent = getIntent();
+        latitude = fromIntent.getStringExtra("Lat");
+        longitude = fromIntent.getStringExtra("Lon");
+        country = fromIntent.getStringExtra("Country");
+        provinceName = fromIntent.getStringExtra("Province");
+        countryCode = fromIntent.getStringExtra("CountryCode");
+        caseNumber = fromIntent.getStringExtra("Cases");
+        date = fromIntent.getStringExtra("Date");
+        id = fromIntent.getLongExtra("id", -1);
+
+        TextView countryTextView = findViewById(R.id.country_text_view);
+        TextView countryCodeTextView = findViewById(R.id.countrycode_text_view);
+        TextView provinceTextView = findViewById(R.id.province_text_view);
+        TextView caseTextView = findViewById(R.id.case_text_view);
+        TextView dateTextView = findViewById(R.id.date_text_view);
+        TextView latitudeTextView = findViewById(R.id.latitude_text_view);
+        TextView longitudeTextView = findViewById(R.id.longitude_text_view);
+
+        countryTextView.setText(country);
+        countryCodeTextView.setText(countryCode);
+        provinceTextView.setText(provinceName);
+        caseTextView.setText(caseNumber);
+        dateTextView.setText(date);
+        latitudeTextView.setText(latitude);
+        longitudeTextView.setText(longitude);
+
+    }
+}
